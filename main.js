09dc8c7e30b509fe06a6536a2aebf676f91d2100
mike242424 <108441006+mike242424@users.noreply.github.com>
2022-09-04 07:42:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 // The forecast today is 293 kelvin
 const kelvin = 283;
+
 // Converting Kelvin to Celsius
 const celsius = kelvin - 273;
+
 // Converting Celsius to Fahrenheit
-let fahrenheit = celsius * (9 / 5) + 32;
 // Using the .floor object to round down the Fahrenheit temperature
-fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+
 // Console log and string interpolation
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 // Converting Celsius to Newton
-let newton = celsius * (33 / 100);
-newton = Math.floor(celsius * (33 / 100));
+// Using the .floor object to round down the Newton temperature
+const newton = Math.floor(celsius * (33 / 100));
+
 // Console log and string interpolation
 console.log(`The temperature is ${newton} degrees Newton.`);
